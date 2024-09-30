2ed8be6b0e103fff815ec3bf02fc2ae9c7a17be8
ChubYury <yurichubuser@gmail.com>
2024-09-30 11:51:31 +0200

    add new routes

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,7 +1,11 @@
 export const server = (done) => {
     app.plugins.browsersync.init({
         server: {
-            baseDir: `${app.path.build.html}`
+            baseDir: `${app.path.build.html}`,
+            index: 'page-home.html',
+            routes: {
+                "/index.html": `${app.path.build.html}/404.html`
+            }
         },
         notify: false,
         port: 3000,
