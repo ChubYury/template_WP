c7345725e392385a1465b5b6b2d2b31e7f422088
ChubYury <yurichubuser@gmail.com>
2024-10-26 16:36:55 +0200

    added testim block and him adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import * as flsFunctions from "./modules/webpTest.js"
-
+import "./modules/slider.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   flsFunctions.isWebp();
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,3 +1,16 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import 'slick-carousel';
+import $ from 'jquery';
 
-const swiper = new Swiper();
+const goodsOption = {
+
+  arrows: true,
+  prevArrow: '<button type="button" class="testim_slider-btn testim_slider-btn--prev"><div class="testim_slider-btn-body testim_slider-btn-body--prev"></div></button>',
+  nextArrow: '<button type="button" class="testim_slider-btn testim_slider-btn--next"><div class="testim_slider-btn-body testim_slider-btn-body--next"></div></button>',
+
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  mobileFirst: true,
+}
+
+$('.js-slider').slick(goodsOption);
