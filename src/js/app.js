526bcbbc960a11d22f7aa59a42af079197d91712
ChubYury <yurichubuser@gmail.com>
2024-10-30 17:57:01 +0100

    added connected js modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
 import * as flsFunctions from "./modules/webpTest.js"
 import "./modules/slider.js";
+import { openFooter } from "./modules/buttons.js"
 
 document.addEventListener('DOMContentLoaded', () => {
   flsFunctions.isWebp();
   
   //--- My modules ------
-
+  openFooter();
 })
